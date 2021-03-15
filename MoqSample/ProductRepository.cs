b395f91b996edb0b7b33a6ad3fdb8903bad546f5
TangYM <Yuanzhi.Tang@cn.unisys.com>
2021-03-15 22:44:50 +0800

    Add CallBaseTest

---
+++ MoqSample/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoqSample
+{
+	public class ProductRepository
+	{
+		public virtual IEnumerable<Product> GetAllProducts()
+		{
+			return new List<Product>()
+			{
+				new Product(){Name="Product in Redis"}
+			};
+		}
+
+		public virtual IEnumerable<Product> GetProductsByType(string type)
+		{
+			return GetAllProducts().Where(p => p.Type == type);
+		}
+	}
+
+	public class Product
+	{
+		public string Name { get; set; }
+
+		public string Type { get; set; }
+	}
+}
---
+++ UnitTest/CallBaseTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MoqSample;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTest
+{
+	[TestClass]
+	public class CallBaseTest
+	{
+		[TestMethod]
+		public void InvokeBaseMethodIfCallBaseEqualsToTrue()
+		{
+			var mock = new Mock<ProductRepository>() { CallBase = true };
+			mock.Setup(r => r.GetAllProducts()).Returns(new List<Product>()
+														{
+															new Product(){Name="p1",Type="Type1"},
+															new Product(){Name="p2",Type="Type2"},
+															new Product(){Name="p3",Type="Type3"}
+														});
+			
+			var result = mock.Object.GetProductsByType("Type1");
+			
+			Assert.AreEqual(1, result.Count());
+		}
+	}
+}
