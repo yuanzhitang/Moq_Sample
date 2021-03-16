7a74b866b09879eec6e822f8d0f88166c5ad9a80
TangYM <Yuanzhi.Tang@cn.unisys.com>
2021-03-16 22:02:16 +0800

    add MockProtectedMethodsTest

---
+++ MoqSample/Command.cs
@@ -0,0 +1,38 @@
+﻿namespace MoqSample
+{
+	public abstract class CommandBase
+	{
+		public string PrintResult()
+		{
+			return $"{Execute()}:{Execute("")}";
+		}
+
+		protected abstract int Execute();
+
+		protected abstract bool Execute(string arg);
+	}
+
+
+	public class Command : CommandBase
+	{
+		//protected virtual int Execute()
+		//{
+		//	return 100;
+		//}
+
+		//protected virtual bool Execute(string arg)
+		//{
+		//	return true;
+		//}
+
+		protected override int Execute()
+		{
+			return 100;
+		}
+
+		protected override bool Execute(string arg)
+		{
+			return true;
+		}
+	}
+}
---
+++ UnitTest/MockProtectedMethodsTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.Protected;
+using MoqSample;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest
+{
+	[TestClass]
+	public class MockProtectedMethodsTest
+	{
+		[TestMethod]
+		public void MockProtectedMethodInAbstractClass()
+		{
+			var mock = new Mock<CommandBase>();
+
+			mock.Protected().Setup<int>("Execute").Returns(99);
+			mock.Protected().Setup<bool>("Execute", ItExpr.IsAny<string>()).Returns(false);
+
+			Assert.AreEqual("99:False", mock.Object.PrintResult());
+		}
+
+		[TestMethod]
+		public void MockProtectedMethodInChildClass()
+		{
+			var mock = new Mock<Command>();
+
+			mock.Protected().Setup<int>("Execute").Returns(88);
+			mock.Protected().Setup<bool>("Execute", ItExpr.IsAny<string>()).Returns(false);
+
+			Assert.AreEqual("88:False", mock.Object.PrintResult());
+		}
+	}
+}
